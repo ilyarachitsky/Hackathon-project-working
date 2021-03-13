f5033cbaeaf412253639be4a97dc75b361662c87
Hugo Silva <hugofsilvaa@gmail.com>
2021-03-13 08:31:56 +0000

    slide menu

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,30 +1,46 @@
 import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
-import Logo from "./Logo/vaccine.png";
+import { SidebarData } from "./SidebarData";
+import "./Navbar.css";
+import { IconContext } from "react-icons";
 
-export default function NavBar() {
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
   return (
-    <div className='NavBar'>
-      <div className='logo'>
-        <Link to='/'>
-          <img src={Logo} alt='Logo' />
-        </Link>
-        <h2>Vaccinator</h2>
-      </div>
-      <div className='links'>
-        <Link to='/' className='link-text'>
-          Home
-        </Link>
-        <Link to='/map' className='link-text'>
-          Map
-        </Link>
-        <Link to='/contact' className='link-text'>
-          Contact
-        </Link>
-        <Link to='/about' className='link-text'>
-          About
-        </Link>
-      </div>
-    </div>
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
   );
 }
+
+export default Navbar;
---
+++ src/Components/SidebarData.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as BiIcons from "react-icons/bi";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Map",
+    path: "/map",
+    icon: <BiIcons.BiMap />,
+    cName: "nav-text",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+    icon: <IoIcons.IoMdContact />,
+    cName: "nav-text",
+  },
+  {
+    title: "About",
+    path: "/about",
+    icon: <IoIcons.IoMdPeople />,
+    cName: "nav-text",
+  },
+];
